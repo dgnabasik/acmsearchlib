18e342bfcacaef5e56d7d584e7b9ba33764d7616
dgnabasik <DavidGnabasik@comcast.net>
2020-12-07 15:04:32 -0700

    Release 1.0.0

--- database/database.go
+++ database/database.go
@@ -68,13 +68,12 @@ func GetDatabaseReference() (*sql.DB, error) {
 		dbPassword    = "Ski7Vail!"
 		dbName        = "postgres"
 		dbDriver      = "postgres"
-		dbSchema      = "acmsearch"
+		dbSchema      = "acmsearch,public"
 		dbConnections = 10
 	)
 
 	dbConn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s search_path=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbPassword, dbName, dbSchema)
-	fmt.Println(dbConn)
 	db, err := sql.Open(dbDriver, dbConn)
 	CheckErr(err)
 	db.SetMaxIdleConns(0)
